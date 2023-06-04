1691c647a63030113e18b3a3532b6f452ab71134
amandaellekilde <amandaellekilde@outlook.com>
2023-06-04 18:48:59 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -12,10 +12,10 @@ class Model:
         self.valid = None
         self.patientCPRs = []
 
-    def authPatient(self, data):                                        #Funktion som validerer, at CPR består af 10 tal
+    def authPatient(self, data):                                        #OBS! Funktion som validerer, at CPR består af 1 tal. SKAL ÆNDRES TIL 10!
         try:
             int(data["cpr"])
-            if (len(data["cpr"]) == 10):
+            if (len(data["cpr"]) == 1):
                 self.valid = "Yes"
                 if (data["cpr"] not in self.patientCPRs):               #hvis cpr ikke er i listen self.patientCPRs, tilføjes det
                     self.patientCPRs.append(data["cpr"])
@@ -37,7 +37,7 @@ class Root(Tk):
 
         #Sætter tid og dato i bunden af skærmen
         self.Timelabel=tk.Label(self, font=("Helvetica bold", 10))
-        self.Timelabel.grid(row = 1, column = 0, sticky = E, padx=40)
+        self.Timelabel.grid(row = 1, column = 0, sticky = E, padx=48)
         def digitalclock():                                                                 #opretter funktion for at kunne vise tiden 
             text_input = time.strftime("%H:%M:%S")                                          #viser tiden i timer, minutter og sekunder og gemmer det som en variable
             self.Timelabel.config(text=text_input)
@@ -47,7 +47,7 @@ class Root(Tk):
         self.date = dt.datetime.now()                                                       #kalder på funktionen der viser tiden og gemmer den i variablen
         self.format_date = f"{self.date:%A, %B %d, %Y}"                                     #viser datoen i dag, måned, dato og år 
         self.Datelabel=tk.Label(self, text = self.format_date,  font=("Helvetica bold", 10))
-        self.Datelabel.grid(row = 1,  column = 0, sticky = E, padx=110)
+        self.Datelabel.grid(row = 1,  column = 0, sticky = E, padx=120)
 
 class View:
     def __init__(self):
@@ -113,8 +113,9 @@ class EKGView(Frame):
         self.grid_columnconfigure(0, weight=2)
         self.grid_columnconfigure(1, weight=1)
 
-        self.header = Label(self, text="EKG for patient", font=("Helvetica bold", 30))
-        self.header.grid(row=0, column=0, pady=70, sticky=S, columnspan=2)
+        self.patientName=tk.StringVar()
+        self.showPatientName = tk.Label(self, font=("Helvetica bold", 30), textvariable=self.patientName)
+        self.showPatientName.grid(row=0, column=0, pady = 70, sticky=S, columnspan=2)
 
         self.EKG = tk.Label(self, text = "GRAF", font=("Segoe UI",150))
         self.EKG.grid(row=1, column=0, rowspan=2)
@@ -125,13 +126,25 @@ class EKGView(Frame):
         self.HRLabel = Label(self.HRFrame, text="Puls:", font=("Sergoe UI", 30))
         self.HRLabel.grid(row=0, column=0, padx=20, pady=20)
 
-        self.HRValue = Label(self.HRFrame, text="60", font=("Sergoe UI", 30))
-        self.HRValue.grid(row=0, column=1, padx=20, pady=20)
+        self.HRValue=tk.StringVar()
+        self.HRValue.set("70")
+        self.HRValueLabel = Label(self.HRFrame, text="60", font=("Sergoe UI", 30), textvariable=self.HRValue)
+        self.HRValueLabel.grid(row=0, column=1, padx=20, pady=20)
 
         self.button = tk.Button(self, text="Se data for ny patient", bg="white", font=("Segoe UI",14))
         self.button.grid(row=2, column=1, ipadx=15, ipady=5)
 
 class Controller:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+        self.ekgController = EKGController(model, view)
+        self.patientController = PatientController(model, view)         
+
+    def start(self):
+        self.view.startMainloop()
+
+class PatientController:
     def __init__(self, model, view):
         self.model = model
         self.view = view
@@ -141,10 +154,6 @@ class Controller:
     
     def _bind(self):
         self.patientFrame.button.config(command=self.showEKGPage)
-        self.EKGFrame.button.config(command=self.showPatientPage)
-
-    def showPatientPage(self):
-        self.view.showPage("Patient")
 
     def showEKGPage(self):
         cpr = self.patientFrame.CPREntry.get()
@@ -152,11 +161,21 @@ class Controller:
         data = {"cpr": cpr, "name": name}
         self.model.authPatient(data)
         if self.model.valid == "Yes":
+            self.EKGFrame.patientName.set("EKG for " + name)
             self.view.showPage("EKG")
 
-
-    def start(self):
-        self.view.startMainloop()
+class EKGController:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+        self.EKGFrame = self.view.frames["EKG"]
+        self._bind()
+    
+    def _bind(self):
+        self.EKGFrame.button.config(command=self.showPatientPage)
+    
+    def showPatientPage(self):
+        self.view.showPage("Patient")
 
 def Main():
     model = Model()
