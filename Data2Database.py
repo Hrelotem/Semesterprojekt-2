603d20d77d752a2a86ca22d9ea14b2543d8f988c
amandaellekilde <amandaellekilde@outlook.com>
2023-06-11 13:29:50 +0200

    Blablabla

--- Data2Database.py
+++ Data2Database.py
@@ -4,22 +4,36 @@ import random
 from threading import Thread, Lock, Condition
 import threading
 import serial 
-import numpy as np
+import sqlite3
+
+"""
+OBS ift. koden:
+Database-klassen er meget ufærdig. Lige nu indsætter den kun én værdi fra bufferen i databasen og ikke hele bufferen.
+Den indsætter alle værdier i databasen knyttet til nummeret 1 i stedet for et autogenereret nummer.
+Den indsætter ingen patientdata sammen med EKG-værdierne.
+Man skal manuelt slette databasen på sin pc, hver gang man gerne vil køre programmet igen.
+Koden skal indsættes i try/except/finally-blokke.
+
+Spørgsmål:
+1) Er notify/wait/lock brugt rigtigt? Seems like it, but not sure
+2) Hvorfor returnerer get-metoden en None sammen med hver buffer???
+3) Hvordan indsætter man en liste i databasen? Det er vel meningen, at vi netop ikke skal splitte listen/bufferen op?
+
+"""
+
+
+#Sensor-klassen opretter en buffer, henter værdier fra sensoren og lægger værdierne i bufferen
+#Når bufferen er fuld, sendes den over i køen, og der oprettes en ny buffer (processen starter forfra)
 
 class Sensor:
     def __init__(self, queue):
         self.buffer = Buffer()
         self.queue = queue
-        #Når konstruktøren startes, oprettes en buffer
-        #Klassen henter en værdi fra sensoren og lægger den i bufferen
-        #Når bufferen er fuld, skal der oprettes en ny buffer
-        #Sensor-klassen sender bufferen over i køen, når den er fuld
 
     def run(self):
         while True:
             data = round(random.random()*10)
             self.buffer.list.append(data)
-            print(self.buffer.list)
             time.sleep(1)
             if len(self.buffer.list) == self.buffer.Amount:
                 self.queue.put(self.buffer)
@@ -27,71 +41,103 @@ class Sensor:
 
     def calculateHR():
         pass
-        #Det er formodentlig nemmest at beregne pulsen i denne klasse"""
-
-#class Sensor(threading.Thread):
-#    def __init__(self):
-#        super().__init__()
-#        pass
-#
-#    def run(self):
-#        while True:
-#            data = np.random.randint(0, 10, 10)
-#            print(data)
-#            time.sleep(1)
+        #Det er formodentlig nemmest at beregne pulsen i denne klasse
 
 class Buffer:
     def __init__(self):
         self.list = []
         self.Amount = 5
 
+#Kø-klassen indeholder en kø af buffere, som ikke er sendt videre til databasen
+#Lock-objekt skal forhindre, at put() og get() udføres samtidig
+
 class Queue:
     def __init__(self):
         self.queue = []
         self._lock = Condition()
         self.bufferCount = 0
-        #Vi skal bruge lock-objektet på put() og get(), så de ikke kan udføres samtidig
-        #Køen indeholder de buffere, som ikke er sendt videre til databasen
     
     def put(self, buffer):
-        #modtager en buffer
-        #tæller bufferCount én op, når den modtager en buffer
+        #modtager en buffer og tæller bufferCount én op
         #sender en notify, hvis den tæller bufferCount én op (vækker get())
         with self._lock:
             self.queue.append(buffer.list)
             self.bufferCount += 1
-            print(self.queue, self.bufferCount)
             self._lock.notify()
-    
-    def get(self):
-        #undersøger, om værdien i køens tæller er større end 0. Hvis den er, kalder den en wait()
-        #piller den buffer ud af kø-listen, der har ligget i listen i længst tid, og returnerer den
-        #tæller bufferCount én ned, når den returnerer en buffer
-        with self._lock:
-            if self.bufferCount > 0:
-                print("Buffercount > 0")
 
+    def get(self):
+        #undersøger, om værdien i bufferCount er større end 0
+        #hvis bufferCount > 0 returneres den buffer i køen, som har ligget der længst
+        #+ der kaldes en wait() + bufferCount tælles én ned
+        with self._lock:
+            if self.bufferCount == 0:
+                self._lock.wait()
+            else:
+                self.bufferCount = self.bufferCount - 1
+                return self.queue.pop(0)
+
+                
 class Database:
     def __init__(self, queue):
         self.queue = queue
 
     def run(self):
+        self.createEKGTable = "CREATE TABLE EKGTable(Number INTEGER, Value INTEGER)"
+        self.insertEKG = "INSERT INTO EKGTable VALUES ({}, {})"
+        self.connection = sqlite3.connect("patientValues.db")
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute(self.createEKGTable)
+        
         while True:
-            self.queue.get()
-            print("Hey")
-            time.sleep(5)
+            data = self.queue.get()
+            if data != None:
+                print(data)
+                test = data[0]
+                self.cursor.execute(self.insertEKG.format(1, test))
+                self.connection.commit()
+
+"""
+class Database:
+    def __init__(self, queue):
+        self.queue = queue
+
+    def run(self):
+        try:
+            dropEKGTable = "DROP TABLE IF EXISTS EKGTable"
+            self.cursor.execute(dropEKGTable)
+            self.createEKGTable = "CREATE TABLE EKGTable(Number INTEGER, Value INTEGER)"
+            self.insertEKG = "INSERT INTO EKGTable VALUES ({}, {})"
+            self.connection = sqlite3.connect("patientValues.db")
+            self.cursor = self.connection.cursor()
+
+            self.cursor.execute(self.createEKGTable)
+        
+            while True:
+                data = self.queue.get()
+                if data != None:
+                    print(data)
+                    test = data[0]
+                    self.cursor.execute(self.insertEKG.format(1, test))
+                    self.connection.commit()
+        
+        except sqlite3.Error as e:
+            print("Kommunikationsfejl med SQLite", e)
+        
+        #finally:
+        #    self.cursor.close()
+        #    self.connection.close()
+"""
 
 
 def Main():
     Q1 = Queue()
     sensor = Sensor(Q1)
     database = Database(Q1)
-    t = threading.Thread(target=sensor.run)
-    t.start()
-    t1 = threading.Thread(target=database.run)
+    t1 = threading.Thread(target=sensor.run)
     t1.start()
-    #t = Sensor()
-    #t.start()
+    t2 = threading.Thread(target=database.run)
+    t2.start()
 
 if __name__ == "__main__":
     Main()
